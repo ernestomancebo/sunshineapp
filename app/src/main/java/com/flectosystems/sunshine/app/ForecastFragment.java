6a1cc53ca4459c830bbe1286ba94b6681853bfa0
Ernesto Manebo <ernesmancebo@gmail.com>
2015-02-21 00:46:17 -0400

    Started unit 3

--- app/src/main/java/com/flectosystems/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/flectosystems/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.flectosystems.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -70,7 +72,16 @@ public class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = adapter.getItem(position);
 
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailIntent);
+            }
+        });
         return rootView;
     }
 
